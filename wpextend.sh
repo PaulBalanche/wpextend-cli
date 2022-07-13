ccaae2d710e7a1731aea08fc2b05250cbd7f415b
Paul Balanche <pb@buzzbrothers.ch>
2022-07-13 17:03:13 +0200

    Up

--- wpextend.sh
+++ wpextend.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-source cli/inc/library.sh
+source include/inc/library.sh
 
 CONFIG_FILE=$PWD/.config
 test -f $CONFIG_FILE || touch $CONFIG_FILE
 source $CONFIG_FILE
 
-COMMANDS_PATH="cli/commands"
+COMMANDS_PATH="include/commands"
 
 if [ -f $COMMANDS_PATH/$1/index.sh ]
 then
