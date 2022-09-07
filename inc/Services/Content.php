8fb353782afc465429a5f72161e566b0930a5188
Paul Balanche <pb@buzzbrothers.ch>
2022-09-07 16:10:50 +0200

    Up

--- inc/Services/Content.php
+++ inc/Services/Content.php
@@ -4,13 +4,22 @@ namespace Wpextend\Cli\Services;
 
 use Wpextend\Cli\Helpers\Render;
 use Wpextend\Cli\Helpers\Terminal;
+use Wpextend\Cli\Controllers\Main;
 
 class Content extends ServiceBase {
 
     public function downloadUploads() {
 
-        $remote_ssh_host = Terminal::readline( 'Remote SSH HOST: ', false );
-        $remote_ssh_user = Terminal::readline( 'Remote SSH USER: ', false );
+        $remote_environment = Main::getInstance()->environmentsController->choose_remote_environment();
+
+        $remote_ssh_host = $this->get_config()->get_data( [ 'env', 'remote', $remote_environment, 'ssh', 'host' ], '[' . $remote_environment . '] Remote SSH HOST' );
+        $remote_ssh_user = $this->get_config()->get_data( [ 'env', 'remote', $remote_environment, 'ssh', 'user' ], '[' . $remote_environment . '] Remote SSH USER' );
+
+        if( empty($remote_ssh_host) || empty($remote_ssh_user) ) {
+
+            Render::output( 'Some information seems empty... Please provide correct SSH credentials.' , 'error' );
+            return false;
+        }
 
         $remote_source_path = rtrim( Terminal::readline( 'Source path (remote): ', false), '/' );
 
